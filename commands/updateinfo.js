6a4dd4878982c40b86a061d4cb80c460f234358e
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2020-12-19 16:11:58 -0600

    Update updateinfo.js
    updated file for new big update

--- commands/updateinfo.js
+++ commands/updateinfo.js
@@ -8,9 +8,9 @@ module.exports = {
         .setDescription('There is a new update so here is some info')
         .addFields(
 
-            {name: 'UBE part', value: 'this add commands shuch as UBE error message command not defined'},
+            {name: 'Overhall part', value: 'overhaled a lot of commands and made alot of embeds'},
             {name: 'Library part', value: 'more sofisticated library has been added to the bot use *librarycommandlist'},
-            {name: 'extras', value: 'expiremetal moderation commands are bieng added and are added'}
+            {name: 'extras', value: 'added moderation commads,(needs admin to work)\n(ban, kick, purge(delete messages))\n also added make an emebd use *HMAE,'}
         )
         .setImage('https://lh3.googleusercontent.com/proxy/nNLG_Dtei3DXYuxP0QmYTWrHMqZ29WY1OTJaNB9X9K9FkNt7P0-897rEyslWvOSA8QoAOG7r4m7bwbkf1PJ-CY16ZdrLoEjnvQ')
         .setFooter('Update info for Tubabot')
