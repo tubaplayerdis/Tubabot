f083d85e9a22f2c3e1491e4be11121444583262d
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2020-12-26 03:15:32 +0000

    more fuck ups
    stop

--- tubabottest.js
+++ tubabottest.js
@@ -335,7 +335,7 @@ client.on('message', message =>{
            message.react('👎') 
         } else if (message.content.startWith('*yes')){
             message.chantent.send('no')
-        } else if (message.content.startsWith('*hostinginfo'){
+        } else if (message.content.startsWith('*hostinginfo')){
             message.channel.send('hosted on a rasberry pi 400')
         }
         
