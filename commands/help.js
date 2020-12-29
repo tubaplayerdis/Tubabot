df09ba4386ffc37af7b0d5f3b9ae6a273404c74f
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2020-12-28 20:23:33 -0600

    more capitalization

--- commands/help.js
+++ commands/help.js
@@ -5,13 +5,13 @@ module.exports = {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#1500FF')
         .setTitle('Help')
-        .setDescription('help commands for peeps who need help!')
+        .setDescription('Help commands for peeps who need help!')
         .addFields(
 
             {name: 'Library help', value: 'use *librarycommandlist for help'},
-            {name: 'command help', value: 'use *commandlist for commands'},
-            {name: 'extra help', value: 'use *infocommandlist. join server for more help, https://discord.gg/gMWaasvj8M'},
-            {name: 'invite bot', value: 'https://discord.com/oauth2/authorize?client_id=775128224224509963&scope=bot&permissions=8'}
+            {name: 'Command help', value: 'use *commandlist for commands'},
+            {name: 'Extra help', value: 'use *infocommandlist. join server for more help, https://discord.gg/gMWaasvj8M'},
+            {name: 'Invite bot', value: 'https://discord.com/oauth2/authorize?client_id=775128224224509963&scope=bot&permissions=8'}
         )
         .setImage('https://childadolescentpsych.cumc.columbia.edu/sites/default/files/styles/555x315/public/HelpFriend-HelpSign-613244854%20555x315.jpg?itok=IzH1dmRK')
         .setFooter('help command for Tubabot')
