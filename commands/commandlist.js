65efb473f149d6531b64776b973655be63d8b760
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2021-01-04 20:54:15 +0000

    Update commandlist.js

--- commands/commandlist.js
+++ commands/commandlist.js
@@ -4,14 +4,16 @@ module.exports = {
     execute(message, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#1500FF')
-        .setTitle('Generalcommandlist')
+        .setTitle('General commandlist')
         .setDescription('for help with general commands')
         .addFields(
-
-            {name: '1st part of commands', value: ' 1.ping, 2.youtube, 3.pingeveryone'},
-            {name: '2nd part of commands', value: '4.help, 5.timeout 6.suggest, 7.UBE'},
-            {name: '3rd part of commands', value: ' 8.bubblewrap, 9.say'},
-            {name: 'extracommandlists', value:'10.brockcommandlist, 11.uselesscommandlist, 12.libraycommandlsit, 13.commandlist_Info, 14.updateinfo'}
+            {name: 'Info', value: 'all comamnds start with *'},
+            {name: 'help menu', value: '1 help \n 2 commandlist \n 3 infocommandlist \n 4 librarycommandlist \n 5 libraryinfo'},
+            {name: 'general commands', value: ' 1 ping \n 2 youtube \n 3 pingeveryone \n 4 help \n 5 timeout \n 6 suggest \n 7 UBE \n 8 bubblewrap \n 9 say'},
+            {name: 'economy', value: '1 work \n 2 bal \n 3 leaderboard \n 4 daily \n 5 store \n 6 buy \n 7 inventory'},
+            {name: 'moderation', value: '1 ban \n 2 kick \n 3 purge(delete messages) \n 4 warn \n 5 checkwarns \n 6 deletewarns'},
+            {name: 'cool commands', value: '1 MAE \n 2 avatar \n 3 ascii \n 4 weather \n 5 meme'},        
+            {name: 'extracommandlists', value:'10 brockcommandlist \n 11 uselesscommandlist \n 12 libraycommandlsit  \n 13 infocommandlist  \n 14 updateinfo'}
         )
         .setImage('https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/Clipboard.svg/1024px-Clipboard.svg.png')
         .setFooter(' generalcommandlist for Tubabot')
