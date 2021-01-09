95be84bf2067c7ffa948843b018a9d3e77ade82b
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2021-01-09 20:36:40 +0000

    Update commandlist.js

--- commands/commandlist.js
+++ commands/commandlist.js
@@ -12,7 +12,7 @@ module.exports = {
             {name: 'General commands', value: ' 1 ping \n 2 youtube \n 3 pingeveryone \n 4 help \n 5 timeout \n 6 suggest \n 7 UBE \n 8 bubblewrap \n 9 say'},
             {name: 'Economy', value: '1 work \n 2 bal \n 3 leaderboard \n 4 daily \n 5 store \n 6 buy \n 7 inventory \n 8 give \n 9 remove'},
             {name: 'Moderation', value: '1 ban \n 2 kick \n 3 purge(delete messages) \n 4 warn \n 5 checkwarns \n 6 deletewarns'},
-            {name: 'Cool commands', value: '1 MAE \n 2 avatar \n 3 ascii \n 4 weather \n 5 meme'},
+            {name: 'Cool commands', value: '1 MAE \n 2 avatar \n 3 ascii \n 4 weather \n 5 meme \n 6 Trump \n 7 jimp'},
             {name: 'Utility', value: '1 checkinvite'},        
             {name: 'Extracommandlists', value:'10 brockcommandlist \n 11 uselesscommandlist \n 12 libraycommandlsit  \n 13 infocommandlist  \n 14 updateinfo'}
         )
