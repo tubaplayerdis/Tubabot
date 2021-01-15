362326a2a790307ce053fb6a26d443edfe718621
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2021-01-15 14:20:40 +0000

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -4,11 +4,21 @@ module.exports = {
     execute(message, args){
         const target = message.mentions.users.first();
         if(message.member.permissions.has("KICK_MEMBERS")){
-            message.channel.send('you have perms')
+           
             if(target){
                 const memberTarget = message.guild.members.cache.get(target.id);
                 memberTarget.kick();
-                message.channel.send("User has been kicked");
+                let reason = args.slice(1).join(" ");
+                if(!reason) reason = 'Unspecified';
+
+                const author = message.author;
+                const banembed = new Discord.MessageEmbed()
+                .setColor('#FF4500')
+                .setTitle('KICK')
+                .setDescription(`${author} kicked ${memberTarget}`)
+                .setFooter(`Reason: ${reason}`)
+                memberTarget.send(`you were kicked in ${message.guild.name} for the follwoing reason: \`${reason}\``)
+                message.channel.send(banembed);
             }else{
                 message.channel.send(`error detected, user cannot be kicked, check user perms`);
             }
