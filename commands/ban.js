2b73687349d163c8c7b83d96062234935dffe460
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2021-01-15 14:22:14 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,11 +4,20 @@ module.exports = {
     execute(message, args){
         const target = message.mentions.users.first();
         if(message.member.permissions.has("BAN_MEMBERS")){
-            message.channel.send('you have perms')
             if(target){
                 const memberTarget = message.guild.members.cache.get(target.id);
                 memberTarget.ban();
-                message.channel.send("User has been Banned");
+                let reason = args.slice(1).join(" ");
+                if(!reason) reason = 'Unspecified';
+
+                const author = message.author;
+                const banembed = new Discord.MessageEmbed()
+                .setColor('#FF4500')
+                .setTitle('BAN')
+                .setDescription(`${author} banned ${memberTarget}`)
+                .setFooter(`Reason: ${reason}`)
+                memberTarget.send(`you were banned in ${message.guild.name} for the follwoing reason: \`${reason}\``)
+                message.channel.send(banembed);
             }else{
                 message.channel.send(`error detected, user cannot be Banned, check user perms`);
             }
