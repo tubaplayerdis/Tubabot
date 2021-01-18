aa8aed092bf2abeecbca7fab6871b5d51de09b94
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2021-01-18 17:22:41 -0600

    Update commandlist.js

--- commands/commandlist.js
+++ commands/commandlist.js
@@ -11,7 +11,7 @@ module.exports = {
         .setDescription('for help with general commands')
         .addFields(
             {name: 'Info', value: 'all comamnds start with *'},
-            {name: 'Help menu', value: '1 help \n 2 commandlist \n 3 infocommandlist \n 4 librarycommandlist \n 5 libraryinfo'},
+            {name: 'Help menu', value: '1 help \n 2 commandlist \n 3 infocommandlist'},
             {name: 'General commands', value: ' 1 ping \n 2 youtube \n 3 pingeveryone \n 4 help \n 5 timeout \n 6 suggest \n 7 UBE \n 8 bubblewrap \n 9 say'},
             {name: 'Economy', value: '1 work \n 2 bal \n 3 leaderboard \n 4 daily \n 5 store \n 6 buy \n 7 inventory \n 8 give \n 9 remove'},
             {name: 'Moderation', value: '1 ban \n 2 kick \n 3 purge(delete messages) \n 4 warn \n 5 checkwarns \n 6 deletewarns'},
