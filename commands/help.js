27d7a384a48c56c1e2ac31034dc4725b18d09f7b
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2021-01-18 17:26:19 -0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,8 @@ module.exports = {
             
             {name: 'Command help', value: 'use *commandlist for commands'},
             {name: 'Extra help', value: 'use *infocommandlist. join server for more help, https://discord.gg/gMWaasvj8M'},
-            {name: 'Invite bot', value: 'https://discord.com/oauth2/authorize?client_id=775128224224509963&scope=bot&permissions=8'}
+            {name: 'Invite bot', value: 'https://discord.com/oauth2/authorize?client_id=775128224224509963&scope=bot&permissions=126022'},
+            {name: 'Support us', value: 'vote for server:\n https://top.gg/servers/739473530747486238/vote \n vote for bot:\n coming soon \n Join server:\n https://discord.gg/gMWaasvj8M'}
         )
         .setImage('https://childadolescentpsych.cumc.columbia.edu/sites/default/files/styles/555x315/public/HelpFriend-HelpSign-613244854%20555x315.jpg?itok=IzH1dmRK')
         .setFooter('help command for Tubabot')
