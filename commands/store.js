2e95844b478e076ccfae2c0dd7953cccbd66adec
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2021-02-04 09:01:38 -0600

    Update store.js

--- commands/store.js
+++ commands/store.js
@@ -7,7 +7,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
         .setColor('#FF4500')
         .setTitle('Store')
-        .setDescription(`Car 🏎️ - 200 coins \n \n Watch ⏱️ - 250 coins \n \n Computer 💻 - 1,000 coins \n \n Drugs💰 - 10,000 coins \n \n 🛠️Tubapowers🛠️ - 100,000,000`)
+        .setDescription(`Car 🏎️ - 200 coins \n \n Watch ⏱️ - 250 coins \n \n Computer 💻 - 1,000 coins \n \n Drugs🍃 - 10,000 coins \n \n 🛠️Tubapowers🛠️ - 100,000,000`)
         .setTimestamp();
 
         message.channel.send(embed);
