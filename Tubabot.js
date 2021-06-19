9e29662511e546b65d804e9fa51b75379013ef87
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2021-06-19 10:51:46 -0500

    adding a nsfw check to you know what
    I HAVE BIG PP

--- Tubabot.js
+++ Tubabot.js
@@ -233,7 +233,12 @@ client.on('message', message =>{
         } else if (message.content.startsWith('*ntinfo')){
             client.commands.get('ntinfo').execute(message, args, Discord, client)
         } else if (message.content.startsWith('*mastrubationtip')){
-            message.channel.send('Use a banket to mastrubate instead of your hand. put it around your dick and slide it accros your dick, also use a soft blanket')
+            if (message.channel.nsfw){
+                 message.channel.send('Use a banket to mastrubate instead of your hand. put it around your dick and slide it accros your dick, also use a soft blanket')
+            } else {
+                message.channel.send('this command can only be used in a nsfw server')
+            }
+            
         } else if (message.content.startsWith('*day')){
             message.channel.send('https://www.tiktok.com/@brandnoot/video/6974623742974627077?is_copy_url=0&is_from_webapp=v1&sender_device=pc&sender_web_id=6940634231467689477')
         }    
